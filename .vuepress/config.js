40c06051d31d127dd6d9e68d8de54290c4134c56
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2019-10-25 19:12:16 +0200

    Testing vuepress

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,5 +3,26 @@ module.exports = {
   description: 'La información oficial de OSW',
   markdown: {
     lineNumbers: true
+  },
+  themeConfig: {
+    logo: '/assets/img/logo.png',
+    repo: 'OSWeekends/press.osweekends',
+    editLinks: true,
+    editLinkText: 'Editar en GitHub',
+    nextLinks: true,
+    prevLinks: true,
+    lastUpdated: 'Última actualización  ', // string | boolean
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about/' }
+    ],
+    smoothScroll: true,
+    searchPlaceholder: 'Buscar...',
+    // TODO
+    // algolia: {
+    //   apiKey: '<API_KEY>',
+    //   indexName: '<INDEX_NAME>'
+    // }
   }
+
 }
